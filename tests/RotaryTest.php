c291ec69a896870f929b71f41cc8a551fc4c096d
Ben <ben@alwaysblank.dev>
2021-10-01 10:35:13 -0700

    Improve testings for various number types

--- tests/RotaryTest.php
+++ tests/RotaryTest.php
@@ -8,53 +8,124 @@ class RotaryTest extends TestCase
 {
     public $testNumber = '1234567890';
 
+    public function potentialNumbers(?string $type): array
+    {
+        $db = [
+            '1234567890' => [
+                'desc' => 'Valid number with no intl',
+                'normal' => '1234567890',
+                'pretty' => '(123) 456-7890',
+                'href' => 'tel:+11234567890',
+                'simple' => '123 456-7890',
+            ],
+            '1234567' => [
+                'desc' => 'Valid number with no intl or area',
+                'normal' => '1234567',
+                'pretty' => '123-4567',
+                'href' => 'tel:1234567',
+                'simple' => '123-4567',
+            ],
+            '771234567890' => [
+                'desc' => 'Valid number with intl and area',
+                'normal' => '771234567890',
+                'pretty' => '77 (123) 456-7890',
+                'href' => 'tel:+771234567890',
+                'simple' => '77 123 456-7890',
+            ],
+            '12345678' => [
+                'desc' => 'Invalid number that should fail',
+                'normal' => '',
+                'pretty' => '',
+                'href' => 'tel:',
+                'simple' => '',
+            ],
+        ];
+
+        if (!in_array($type, ['normal', 'pretty', 'simple', 'href'])) {
+            return $db;
+        }
+
+        $return = [];
+        foreach ($db as $test => $types) {
+            $return[$types['desc']] = [$test, $types[$type]];
+        }
+
+        return $return;
+    }
+
     public function testCanBeInstantiated(): void
     {
         $rotary = new Rotary($this->testNumber);
         $this->assertInstanceOf(Rotary::class, $rotary);
     }
 
-    public function testPrettyOutput(): void
+    public function providePrettyOutput(): array
     {
-        $rotary = new Rotary($this->testNumber);
-        $this->assertEquals(
-            '(123) 456-7890',
-            $rotary->pretty()
-        );
+        return $this->potentialNumbers('pretty');
     }
 
-    public function testHrefOutput(): void
+    /**
+     * @dataProvider providePrettyOutput
+     */
+    public function testPrettyOutput($test, $expected): void
     {
-        $rotary = new Rotary($this->testNumber);
-        $this->assertEquals(
-            'tel:+11234567890',
-            $rotary->href()
-        );
+        $rotary = new Rotary($test);
+        $this->assertEquals($expected, $rotary->pretty());
     }
 
-    public function testSimpleOutput(): void
+    public function provideHrefOutput(): array
     {
-        $rotary = new Rotary($this->testNumber);
-        $this->assertEquals(
-            '123 456-7890',
-            $rotary->simple()
-        );
+        return $this->potentialNumbers('href');
     }
 
-    public function testNormalizedOutput(): void
+    /**
+     * @dataProvider provideHrefOutput
+     * @param $test
+     * @param $expected
+     */
+    public function testHrefOutput($test, $expected): void
     {
-        $rotary = new Rotary($this->testNumber);
-        $this->assertEquals(
-            '1234567890',
-            $rotary->normalized()
-        );
+        $rotary = new Rotary($test);
+        $this->assertEquals($expected, $rotary->href());
+    }
+
+    public function provideSimpleOutput(): array
+    {
+        return $this->potentialNumbers('simple');
+    }
+
+    /**
+     * @dataProvider provideSimpleOutput
+     * @param $test
+     * @param $expected
+     */
+    public function testSimpleOutput($test, $expected): void
+    {
+        $rotary = new Rotary($test);
+        $this->assertEquals($expected, $rotary->simple());
+    }
+
+    public function provideNormalizedOutput(): array
+    {
+        return $this->potentialNumbers('normal');
+    }
+
+    /**
+     * @dataProvider provideNormalizedOutput
+     * @param $test
+     * @param $expected
+     */
+    public function testNormalizedOutput($test, $expected): void
+    {
+        $rotary = new Rotary($test);
+        $this->assertEquals($expected, $rotary->normalized());;
     }
 
     public function testFormattedOutput(): void
     {
         $rotary = new Rotary($this->testNumber);
         $this->assertEquals(
-            $rotary->format(function($Number) {
+            $rotary->format(function ($Number) {
                 return '%area%!%first%!%second%';
             }),
             '123!456!7890'
