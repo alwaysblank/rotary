28a04fc9393da19b264881d4af0b44782d94379e
Ben <ben@alwaysblank.dev>
2021-10-01 10:35:05 -0700

    Check for invalid numbers while parsing

--- src/Parse.php
+++ src/Parse.php
@@ -45,6 +45,21 @@ class Parse
             return in_array((string)$key, ['intl', 'area', 'first', 'second']);
         }, ARRAY_FILTER_USE_KEY);
 
+        // Can't have a phone number without these.
+        if (!$matches['first'] || !$matches['second']) {
+            return [];
+        }
+
+        // We have to have an area code for an valid intl segment.
+        if (!$matches['area'] && $matches['intl']) {
+            return [];
+        }
+
+        // All digits are important, so fail if any have been trimmed.
+        if (strlen($cleaned) !== strlen(join('', $matches))) {
+            return [];
+        }
+
         // Turn all segments the right way round again.
         return array_map('strrev', $matches);
     }
